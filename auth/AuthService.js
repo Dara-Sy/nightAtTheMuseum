fa7420d33cae4db08ac30694d50075e73cabf86d
Phil Yoo <philyoo@ool-ad020634.dyn.optonline.net>
2018-05-07 17:54:59 -0400

    installed jsonwebtoken

---
+++ auth/AuthService.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+const User   = require('../user/model/User');
+const TokenService = require('./TokenService');
+
+
+const isValidUser = async ({ username, password:textPassword }) => {
+  try {
+    const user = await User.findOne(username);
+    return await bcrypt.compare(textPassword, user.password);
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+module.exports = {
+  authenticate(req, res, next) {
+    if (!isValidUser(req.body)) {
+      return next({});
+    }
+
+    TokenService.makeToken({
+      username: req.body.username,
+      roles:    ['admin', 'editor', 'user'],
+    })
+      .then((token) => {
+        res.locals.token = token;
+        next();
+      })
+      .catch(next);
+    return false;
+  },
+
+  allow({ roles }) {
+    return [
+      (req, res, next) => {
+        TokenService.verify(req.authToken)
+          .then((payload) => {
+            const isAuthorized = roles.some(n => payload.roles.includes(n));
+            return isAuthorized ? next() : Promise.reject('User not authorized');
+          })
+          .catch(next);
+      },
+      (err, req, res, next) => {
+        console.log(err);
+        res.status(403).json({});
+      },
+    ];
+  },
+};
--- server.js
+++ server.js
@@ -3,26 +3,26 @@ const express    = require('express');
 const logger     = require('morgan');
 const path       = require('path');
 
-// start express
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// some logging
+
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(tokenService.receiveToken);
 
-// ROUTE HANDLER
+
 app.use('/api', (req, res) => {
   res.json({ message: 'hello from API' });
 });
 
-// GLOBAL ERROR HANDLER
+
 app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
 
-// START SERVER
 app.listen(PORT, () => {
   console.log(`Server up and listening on port ${PORT}, in ${app.get('env')} mode.`);
 }).on('error', console.error);
