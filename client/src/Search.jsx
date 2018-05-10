c8ff68c10ea2ead0c4006cdf5c6fa190c1e517bf
Lance R Villanueva <lance.villanueva94@gmail.com>
2018-05-10 17:10:03 -0400

    reactL search is rendering stuff

--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -4,15 +4,17 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state= {
-      museum: '',
+      museums: [],
       zipcode: '',
       city: '',
       name: '',
     }
 
     this.getData = this.getData.bind(this);
-    this.newSearch = this.newSearch.bind(this)
+    this.newSearch = this.newSearch.bind(this);
+    this.renderMuseums = this.renderMuseums.bind(this);
   }
+
   //fetch all museuems in search!!
   // componentWillMount() {
   //   fetch(`/search`)
@@ -36,28 +38,36 @@ class Search extends React.Component {
   // }
 newSearch(e) {
   this.setState({
-    museum: e.target.value
+    city: e.target.value
   });
 }
 
 getData(e) {
-  console.log('We are getting data:' + this.state.museum)
+  // console.log('We are getting data:' + this.state.museum)
   e.preventDefault();
-  let zipcode = this.state.zipcode
+  // let zipcode = this.state.zipcode
   // let museum = this.state.museum
-  fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.730610, -73.935242&radius=1500&type=museum&keyword=&key=AIzaSyAffC3lLmIkfN33zhtCmtkJwH7k6TP9EnE`)
+  // fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.730610, -73.935242&radius=1500&type=museum&keyword=&key=AIzaSyAffC3lLmIkfN33zhtCmtkJwH7k6TP9EnE`)
+  // fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=museums+in+${data[0].museum_location}&key=${process.env.API_KEY}`)
+  fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=museums+in+new+york&key=AIzaSyAffC3lLmIkfN33zhtCmtkJwH7k6TP9EnE`)
   .then(res => res.json())
     .then(data => {
       console.log(data);
+      console.log("these are names, " + data.results[0].name);
       this.setState({
-        city: data.results.vicinity,
+        museums: data.results,
         name: data.name,
       })
-      console.log(data.results.name);
     })
     .catch(err =>
       console.log(err)
     )
+}
+renderMuseums(){
+  //.this.state.map
+  return this.state.museums.map(d => {
+    return ( <li>{d.name}</li>)
+  })
 }
 
 
@@ -69,10 +79,12 @@ render(props) {
        type="text"
        name="zipcode"
        placeholder="searchbar"
-       value={this.state.museum}
        onChange={this.newSearch}
        />
        <button onClick={this.getData}>Find Museums</button>
+       <ul>
+       {this.renderMuseums()}
+       </ul>
     </div>
     )
 }
