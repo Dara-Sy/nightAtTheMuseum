d70c0cce60866dbb3d80f5b0c47b3f1dbd61e243
darasy <darajanne@gmail.com>
2018-05-14 13:43:14 -0400

    adding box to the login

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from './Register';
 import Login from './Login';
 import Museums from './Museums';
 import './App.css';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -64,13 +65,13 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div className="Nav">
+        <header>
             <h1>Night At The Museum</h1>
 
             <a className="navlinks" href="/login"> Home </a>
             <a className="navlinks" href="/search"> Search </a>
 
-        </div>
+        </header>
           <main>
             <Switch>
               <Route
@@ -113,12 +114,20 @@ export default class App extends React.Component {
           </main>
 
 
-          <div className="Navimg"> </div>
+
 
           <div className="Footer">
+
             <h4> © Made by the best DALP you'll ever meet</h4>
+
+
+
           </div>
         </div>
     );
   }
 }
+
+
+// <i class="fa fa-car" aria-hidden="true"></i>
+// <span class="fa-twitter fa"></span>
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -37,47 +37,52 @@ export default class Login extends React.Component {
 
   render(props) {
     return (
-      <div className="logInContainer">
-        <tbody>
-          <tr>
-            <td>
-              <p>Username</p>
-            </td>
-            <td>
-              <input
-                className="reginput"
-                type="text"
-                placeholder="username"
-                ref="username"
-                name="username"
-                onInput={this.handleChange}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>Password</p>
-            </td>
-            <td>
-              <input
-                className="reginput"
-                type="password"
-                placeholder="password"
-                ref="password"
-                name="password"
-                onInput={this.handleChange}
-              />
-            </td>
-          </tr>
-        </tbody>
-        <div className="logInButton">
-          <button
-            className="button"
-            type="submit"
-            onClick={this.submitReg}
-          >Submit</button>
-        </div>
-      </div>
+      <main className="Navimg">
+          <div className="box">
+          </div>
+        <div className="logInContainer">
+            <tbody>
+              <tr>
+                <td>
+                  <p>Username</p>
+                </td>
+                <td>
+                  <input
+                    className="reginput"
+                    type="text"
+                    placeholder="username"
+                    ref="username"
+                    name="username"
+                    onInput={this.handleChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>Password</p>
+                </td>
+                <td>
+                  <input
+                    className="reginput"
+                    type="password"
+                    placeholder="password"
+                    ref="password"
+                    name="password"
+                    onInput={this.handleChange}
+                  />
+                </td>
+              </tr>
+            </tbody>
+            <div className="logInButton">
+              <button
+                className="button"
+                type="submit"
+                onClick={this.submitReg}
+              >Submit</button>
+            </div>
+          </div>
+
+      </main>
     );
   }
 }
